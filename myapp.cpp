c3a1fa42a4b33f1e06f64d232da418755c7b4537
tung-sh <tungntfx08789@funix.edu.vn>
2023-03-15 08:26:31 +0700

    update

--- myapp.cpp
+++ myapp.cpp
@@ -3,14 +3,23 @@
 #include "lib/argh.h"
 using namespace std;
 
+
 char a[] = "Hello Tung! ";
 char b[] = "I am your faithful servant! \n";
 char c[] = "My name is Rim! ";
 char d[] = "(^_^)";
 
+char e[] = "Hello Tung! ";
+char f[] = "What can I do for you? (_ _)\n"; 
+char g[] = "1 - \n";
+char h[] = "2 - \n"; 
+
+
+
 int main(int, char* argv[]) {
     argh::parser cmdl(argv);
 
+    // 1. say hello to user
     if (cmdl[{"-h", "--helloRim"}]) {
         type_writing(a, 200); 
         wait_1ms(1000);
@@ -21,6 +30,19 @@ int main(int, char* argv[]) {
         type_writing(d, 0);
         wait_1ms(400); 
     }
+
+    // 2. what can i do for you? 
+    if (cmdl[{"-s", "--serve"}]) {
+        type_writing(e, 100); 
+        wait_1ms(100);
+        type_writing(f, 100); 
+        wait_1ms(100);
+        type_writing(g, 100); 
+        wait_1ms(100);
+        type_writing(h, 100); 
+        wait_1ms(100);
+    }
+    
 
     return 0; 
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
-# Sản phẩm bot chat command line
-1. Đầu tiên bạn hãy compile file myapp.cpp
+# Sản phẩm bot chat command line: RIM
+## Hướng dẫn sử dụng 
+1. Mở file myapp.exe
 2. Gõ trên command line dòng sau ".\myapp -h" Hoặc ".myapp --helloRim" 
+3. Gõ trên command line ".\myapp -s" hoặc ".\myapp --serve" để sai Rim làm việc 
